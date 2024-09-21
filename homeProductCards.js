93ba7c6a99913b2fc84787ab8f00211b9ff09d6b
your-MuhammadMuddasar <your-muhammadmuddasar3786@gmail.com>
2024-09-21 16:15:55 +0500

    Toggle Quantity Added

--- homeProductCards.js
+++ homeProductCards.js
@@ -1,3 +1,7 @@
+import { addToCart } from './addToCart.js';
+import { homeQuantityToggle } from './homeQuantityToggle.js';
+
+
 const productContainer = document.querySelector('#productContainer')
 const productTemplate = document.querySelector('#productTemplate')
 
@@ -13,8 +17,38 @@ products.forEach(curProduct => {
 
 
       const productClone = document.importNode(productTemplate.content, true);
+// --------FOR ADDING TOGGLR-----------
+      productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
     
-      productClone.querySelector('.productName').textcontent = name;
+      productClone.querySelector(".category").textContent = category;
+      productClone.querySelector(".productName").textContent = name;
+      productClone.querySelector(".productImage").src = image;
+      productClone.querySelector(".productImage").alt = name;
+      productClone.querySelector(".productStock").textContent = stock;
+      productClone.querySelector(".productDescription").textContent = description;
+      productClone.querySelector(".productPrice").textContent = `₹${price}`;
+      productClone.querySelector(".productActualPrice").textContent = `₹${
+        price * 4
+      }`;
+
+      
+      // --------FOR ADDING TOGGLR-----------
+        productClone
+          .querySelector(".stockElement")
+          .addEventListener("click", (event) => {
+            homeQuantityToggle(event, id, stock); 
+          });
+
+ // --------FOR ADDING ADD TO CART-----------
+
+    productClone
+    .querySelector('.add-to-cart-button')
+    .addEventListener("click", (event) => {
+       
+      addToCart(event, id, stock);
+
+    })
+        
 
 
       productContainer.append(productClone)
@@ -22,6 +56,17 @@ products.forEach(curProduct => {
 });
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-// import "./style.css";
+// // import "./style.css";
 
-// import products from "./api/products.json";
+// // import products from "./api/products.json";
 
 
-// Use fetch to load the products.json file
-fetch('./api/products.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // Parse the JSON response
-  })
-  .then(products => {
-    // Now you have access to the products data
-    console.log(products);
+// // Use fetch to load the products.json file
+// fetch('./api/products.json')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json(); // Parse the JSON response
+//   })
+//   .then(products => {
+//     // Now you have access to the products data
+//     console.log(products);
 
-    // You can use the products data here in your app
-    // Example: Display product names in the console
-    products.forEach(products => {
-      console.log(products.name);
-    });
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+//     // You can use the products data here in your app
+//     // Example: Display product names in the console
+//     products.forEach(products => {
+//       console.log(products.name);
+//     });
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
 
 
 
@@ -41,3 +41,19 @@ fetch('./api/products.json')
  });
 
  // ---------CARDS ADDED ABOVE-----------
+
+
+//  import { homeQuantityToggle } from './homeQuantityToggle.js'; 
+
+
+
+
+
+
+
+
+
+
+
+
+
